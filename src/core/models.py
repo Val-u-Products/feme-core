4289c184f7dea183357796f5b70a6eb247603329
luisval-u <luis@myval-u.com>
2023-04-19 23:24:37 -0400

    New models

--- src/core/models.py
+++ src/core/models.py
@@ -2,109 +2,129 @@ from django.db import models
 
 
 class ColeTabla(models.Model):
-    uuid_cole = models.TextField(blank=True, null=True)
-    colegio = models.TextField(db_column='Colegio', blank=True, null=True) 
-    pais = models.TextField(db_column='País', blank=True, null=True)  
-    ubicacion = models.TextField(db_column='Ubicacion', blank=True, null=True)  
-    zona_horaria = models.TextField(db_column='Zona Horaria', blank=True, null=True)  
-    sexo_colegio = models.TextField(db_column='Sexo-Colegio', blank=True, null=True)  
-    tipo_de_inscripcion_actual = models.TextField(db_column='Tipo de Inscripcion Actual', blank=True, null=True)  
-    tipo_de_inscripcion_historico = models.TextField(db_column='Tipo de Inscripcion historico', blank=True, null=True)  
-    zoho_link_field = models.TextField(db_column='Zoho Link ', blank=True, null=True)  
+    uuid_cole = models.TextField(primary_key=True, blank=True, null=False, db_column='uuid_cole')
+    colegio = models.TextField(blank=True, null=True)
+    ubicacion = models.TextField(blank=True, null=True)
+    zona_horaria = models.TextField(blank=True, null=True)
+    sexo_colegio = models.IntegerField(blank=True, null=True, db_column='sexo-colegio')
+    zoho_link = models.TextField(blank=True, null=True, db_column='zoho_link_')
 
     class Meta:
-        managed = False
         db_table = 'cole_tabla'
 
 
-class DfSalonKpi(models.Model):
-    colegio = models.TextField(db_column='Colegio', blank=True, null=True)  
-    id_monitor = models.TextField(db_column='ID_Monitor', blank=True, null=True)  
-    monitor = models.TextField(db_column='Monitor', blank=True, null=True)  
-    apellido_monitor = models.TextField(db_column='Apellido Monitor', blank=True, null=True)  
-    grado = models.TextField(db_column='Grado', blank=True, null=True) 
-    seccion = models.TextField(db_column='Sección', blank=True, null=True)  
-    materia_feme = models.TextField(db_column='Materia FEME', blank=True, null=True)  
-    zoho_link = models.TextField(db_column='Zoho Link', blank=True, null=True)  
-    modulos = models.TextField(db_column='Modulos', blank=True, null=True)  
-    salon_mol = models.TextField(db_column='Salon_Mol', blank=True, null=True) 
-    id_salon = models.TextField(db_column='ID_Salon', blank=True, null=True)  
-    inscripcion_correcto_t_l3 = models.BigIntegerField(db_column='Inscripcion_correcto_T_L3', blank=True, null=True)  
-    id_mol = models.TextField(db_column='ID_Mol', blank=True, null=True)  
-    uuid_salon = models.TextField(blank=True, null=True)
-    total_estudiantes = models.BigIntegerField(db_column='Total estudiantes', blank=True, null=True)  
-    iniciaron = models.BigIntegerField(db_column='Iniciaron', blank=True, null=True)  
-    llevan_50_field = models.BigIntegerField(db_column='Llevan +50%', blank=True, null=True)  
-    completaron = models.BigIntegerField(db_column='Completaron', blank=True, null=True) 
-    fecha_insc = models.TextField(db_column='Fecha_Insc', blank=True, null=True)  
-
-    class Meta:
-        managed = False
-        db_table = 'df_salon_kpi'
-
-
 class EstudiantesTabla(models.Model):
-    id_thinki = models.FloatField(db_column='ID_thinki', blank=True, null=True)  
-    uuid_salon = models.TextField(blank=True, null=True)
+    id_thinkific = models.TextField(primary_key=True, blank=True, null=False, db_column='id_thinkific')
     uuid_mont = models.TextField(blank=True, null=True)
-    colegio = models.TextField(db_column='Colegio', blank=True, null=True)  
-    grado = models.TextField(db_column='Grado', blank=True, null=True)  
-    sección = models.TextField(db_column='Sección', blank=True, null=True) 
-    nombres_estudiantes = models.TextField(db_column='Nombres estudiantes', blank=True, null=True)  
-    apellidos_estudiantes = models.TextField(db_column='Apellidos estudiantes', blank=True, null=True)  
-    email = models.TextField(db_column='Email', blank=True, null=True)  
-    whatsapp_estudiante = models.FloatField(db_column='WhatsApp estudiante', blank=True, null=True)  
-    nombres_representante = models.TextField(db_column='Nombres representante', blank=True, null=True)  
-    apellidos_representante = models.TextField(db_column='Apellidos representante', blank=True, null=True)  
-    whatsapp_responsable = models.FloatField(db_column='WhatsApp responsable', blank=True, null=True)  
-    email_representante = models.TextField(db_column='Email representante', blank=True, null=True)  
-    inscrito = models.TextField(db_column='Inscrito', blank=True, null=True)  
-    id_monitor = models.TextField(db_column='ID_Monitor', blank=True, null=True)  
-    lapsos_de_inscripcion_21 = models.TextField(db_column='Lapsos de Inscripcion_21', blank=True, null=True)  
-    lapsos_de_inscripcion_22 = models.TextField(db_column='Lapsos de Inscripcion_22', blank=True, null=True)  
-    q_de_inscripcion_21 = models.TextField(db_column='Q de Inscripcion_21', blank=True, null=True)  
-    q_de_inscripcion_22 = models.TextField(db_column='Q de Inscripcion_22', blank=True, null=True)  
-    q_de_inscripcion_23 = models.TextField(db_column='Q de Inscripcion_23', blank=True, null=True) 
-    sexo = models.TextField(db_column='Sexo', blank=True, null=True)  
+    uuid_cole = models.ForeignKey(ColeTabla, on_delete=models.CASCADE, db_column='uuid_cole')
+    uuid_salon = models.TextField(blank=True, null=True)
+    colegio = models.TextField(blank=True, null=True)
+    grado = models.TextField(blank=True, null=True)
+    seccion = models.TextField(blank=True, null=True)
+    nombres_estudiantes = models.TextField(blank=True, null=True)
+    apellidos_estudiantes = models.TextField(blank=True, null=True)
+    email = models.BooleanField(default=False)
+    whatsapp_estudiante = models.IntegerField(blank=True, null=True)
+    nombres_representante = models.TextField(blank=True, null=True)
+    apellidos_representante = models.TextField(blank=True, null=True)
+    whatsapp_responsable = models.IntegerField(blank=True, null=True)
+    email_representante = models.TextField(blank=True, null=True)
+    inscrito = models.CharField(blank=True, null=True, max_length=255)
 
     class Meta:
-        managed = False
         db_table = 'estudiantes_tabla'
 
 
-class MonitorTabla(models.Model):
-    uuid_mont = models.TextField(blank=True, null=True)
-    monitor = models.TextField(db_column='Monitor', blank=True, null=True)  
-    apellido_monitor = models.TextField(db_column='Apellido Monitor', blank=True, null=True)  
-    materia_feme = models.TextField(db_column='Materia FEME', blank=True, null=True)  
-    field_puntaje = models.FloatField(db_column='% Puntaje', blank=True, null=True)  
-    whatsapp = models.FloatField(db_column='Whatsapp', blank=True, null=True)  
-    email_m = models.TextField(db_column='Email_M', blank=True, null=True)  
-    id_thinki_mon = models.FloatField(db_column='ID_thinki_Mon', blank=True, null=True)  
+class Modulos(models.Model):
+    id_mol = models.IntegerField(primary_key=True, null=False, blank=True, db_column='id_mol')
+    nombre = models.TextField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        db_table = 'modulos'
+
+
+class QuizTabla(models.Model):
+    id_pregunta = models.TextField(primary_key=True, blank=True, null=False, db_column='id_pregunta')
+    id_mol = models.ForeignKey(Modulos, on_delete=models.CASCADE, db_column='id_mol')
+    quiz = models.IntegerField(blank=True, null=True)
+    seccion = models.IntegerField(blank=True, null=True)
+    titulo_de_seccion = models.TextField(blank=True, null=True)
+    numero = models.IntegerField(blank=True, null=True)
+    tipo_de_pregunta = models.TextField(blank=True, null=True)
+    pregunta = models.TextField(blank=True, null=True)
+    respuesta_correcta = models.TextField(blank=True, null=True)
+    respuesta_incorrecta_1 = models.TextField(blank=True, null=True)
+    respuesta_incorrecta_2 = models.TextField(blank=True, null=True)
+    respuesta_incorrecta_3 = models.TextField(blank=True, null=True)
+    respuesta_incorrecta_4 = models.TextField(blank=True, null=True)
+    explicacion_de_la_respueta_correcta = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'quiz_tabla'
+
+
+class MonitorTabla(models.Model):
+    uuid_cole = models.ForeignKey(ColeTabla, on_delete=models.CASCADE, db_column='uuid_cole')
+    uuid_mont = models.TextField(primary_key=True, null=False, blank=True, db_column='uuid_mont')
+    monitor = models.TextField(blank=True, null=True)  
+    materia_feme = models.TextField(blank=True, null=True)  
+    field_puntaje = models.FloatField(db_column='%_puntaje', blank=True, null=True)  
+    whatsapp = models.FloatField(blank=True, null=True)  
+    email_m = models.TextField(blank=True, null=True)  
+    id_thinki_mon = models.FloatField(blank=True, null=True)  
+
+    class Meta:
         db_table = 'monitor_tabla'
 
 
 class SalonTabla(models.Model):
-    uuid_salon = models.TextField(blank=True, null=True)
-    id_monitor = models.TextField(db_column='ID_Monitor', blank=True, null=True)  
-    cierre_1 = models.DateTimeField(db_column='Cierre 1', blank=True, null=True)  
-    cierre_2 = models.DateTimeField(db_column='Cierre 2', blank=True, null=True)  
-    cierre_3 = models.DateTimeField(db_column='Cierre 3', blank=True, null=True)  
+    uuid_salon = models.TextField(primary_key=True, null=False, blank=True)
+    id_monitor = models.ForeignKey(MonitorTabla, on_delete=models.CASCADE, db_column='uuid_mont')
     cierre_definitivo = models.TextField(db_column='Cierre definitivo', blank=True, null=True)  
-    l1_22_23 = models.TextField(db_column='L1-22-23', blank=True, null=True)  
-    l1_22_23_2 = models.TextField(db_column='L1-22-23_2', blank=True, null=True)  
-    l1_22_23_au = models.TextField(db_column='L1-22-23_AU', blank=True, null=True)  
-    l2_22_23 = models.TextField(db_column='L2-22-23', blank=True, null=True)  
-    l2_22_23_2 = models.TextField(db_column='L2-22-23_2', blank=True, null=True)  
-    l2_22_23_au = models.TextField(db_column='L2-22-23_AU', blank=True, null=True)  
-    l2_22_23_au_2 = models.TextField(db_column='L2-22-23_AU_2', blank=True, null=True)  
-    l3_22_23 = models.TextField(db_column='L3-22-23', blank=True, null=True)  
-    l3_22_23_2 = models.TextField(db_column='L3-22-23_2', blank=True, null=True)  
-    l3_22_23_au = models.TextField(db_column='L3-22-23_AU', blank=True, null=True)  
+    l3_22_23 = models.TextField(db_column='l3-22-23', blank=True, null=True)  
+    l3_22_23_2 = models.TextField(db_column='l3-22-23_2', blank=True, null=True)  
+    l3_22_23_au = models.TextField(db_column='l3-22-23_au', blank=True, null=True)  
 
     class Meta:
-        managed = False
         db_table = 'salon_tabla'
+
+
+class SalonKpiModulo(models.Model):
+    uuid_salon = models.ForeignKey(SalonTabla, on_delete=models.CASCADE, db_column='uuid_salon')
+    id_mol = models.ForeignKey(Modulos, on_delete=models.CASCADE, db_column='id_mol')
+    grado = models.TextField(blank=True, null=True)
+    seccion = models.TextField(blank=True, null=True)
+    modulos = models.TextField(blank=True, null=True) 
+    total_estudiantes = models.IntegerField(blank=True, null=True)
+    iniciaron = models.IntegerField(blank=True, null=True)
+    llevan50 = models.IntegerField(blank=True, null=True, db_column='llevan_+50%')
+    completaron = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'salon_kpi_modulo'
+
+
+class MonitorTablaBeta(models.Model):
+    uuid_cole = models.TextField(blank=True, null=True)
+    uuid_mont = models.TextField(primary_key=True, null=False, blank=True)
+    monitor = models.TextField(blank=True, null=True)  
+    materia_feme = models.TextField(blank=True, null=True)  
+    field_puntaje = models.FloatField(db_column='%_puntaje', blank=True, null=True)  
+    whatsapp = models.FloatField(blank=True, null=True)  
+    email_m = models.TextField(blank=True, null=True)  
+    id_thinki_mon = models.FloatField(blank=True, null=True)  
+
+    class Meta:
+        db_table = 'monitor_tabla_beta'
+
+
+class SalonTablaBeta(models.Model):
+    uuid_salon = models.TextField(primary_key=True, null=False, blank=True)
+    id_monitor = models.ForeignKey(MonitorTablaBeta, on_delete=models.CASCADE, db_column='uuid_mont')
+    cierre_definitivo = models.TextField(db_column='Cierre definitivo', blank=True, null=True)  
+    l3_22_23 = models.TextField(db_column='l3-22-23', blank=True, null=True)  
+    l3_22_23_2 = models.TextField(db_column='l3-22-23_2', blank=True, null=True)  
+    l3_22_23_au = models.TextField(db_column='l3-22-23_au', blank=True, null=True)  
+
+    class Meta:
+        db_table = 'salon_tabla_beta'
