35ce1c0db1ec0011f7ae81b9b3961a0f8479902c
luisval-u <luis@myval-u.com>
2023-06-02 11:35:20 -0400

    Update models.

--- src/core/models.py
+++ src/core/models.py
@@ -10,7 +10,7 @@ class ColeTabla(models.Model):
     zoho_link = models.TextField(blank=True, null=True, db_column='zoho_link_')
 
     class Meta:
-        db_table = 'cole_tabla'
+        db_table = 'colegio_tabla'
 
 
 class Modulos(models.Model):
@@ -22,7 +22,8 @@ class Modulos(models.Model):
 
 
 class QuizTabla(models.Model):
-    id_pregunta = models.TextField(primary_key=True, blank=True, null=False, db_column='id_pregunta')
+    id_q = models.IntegerField(primary_key=True, unique=True, db_column='id_q')
+    id_pregunta = models.TextField(blank=True, null=True, db_column='id_pregunta')
     id_mol = models.ForeignKey(Modulos, on_delete=models.CASCADE, db_column='id_mol')
     quiz = models.IntegerField(blank=True, null=True)
     seccion = models.IntegerField(blank=True, null=True)
@@ -35,7 +36,7 @@ class QuizTabla(models.Model):
     respuesta_incorrecta_2 = models.TextField(blank=True, null=True)
     respuesta_incorrecta_3 = models.TextField(blank=True, null=True)
     respuesta_incorrecta_4 = models.TextField(blank=True, null=True)
-    explicacion_de_la_respueta_correcta = models.TextField(blank=True, null=True)
+    explicacion_de_la_respueta_correcta = models.TextField(blank=True, null=True, db_column='explicacion_correcta')
 
     class Meta:
         db_table = 'quiz_tabla'
@@ -46,8 +47,9 @@ class MonitorTabla(models.Model):
     uuid_mont = models.TextField(primary_key=True, null=False, blank=True, db_column='uuid_mont')
     monitor = models.TextField(blank=True, null=True)  
     materia_feme = models.TextField(blank=True, null=True)  
-    field_puntaje = models.FloatField(db_column='per_puntaje', blank=True, null=True)  
-    whatsapp = models.IntegerField(blank=True, null=True)  
+    field_puntaje = models.FloatField(blank=True, null=True, db_column='per_puntaje')  
+    whatsapp = models.IntegerField(blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
     email_m = models.TextField(blank=True, null=True)  
     id_thinki_mon = models.FloatField(blank=True, null=True)
     userToken = models.CharField(max_length=100, unique=True, blank=True, null=True, db_column='userToken')
@@ -57,7 +59,7 @@ class MonitorTabla(models.Model):
 
 
 class EstudiantesTabla(models.Model):
-    id_thinkific = models.TextField(primary_key=True, blank=True, null=False, db_column='id_thinkific')
+    id_thinkific = models.IntegerField(primary_key=True, blank=True, db_column='id_thinkific')
     uuid_mont = models.ForeignKey(MonitorTabla, on_delete=models.CASCADE, db_column='uuid_mont')
     uuid_cole = models.ForeignKey(ColeTabla, on_delete=models.CASCADE, db_column='uuid_cole')
     uuid_salon = models.TextField(blank=True, null=True)
@@ -70,12 +72,12 @@ class EstudiantesTabla(models.Model):
     whatsapp_estudiante = models.IntegerField(blank=True, null=True)
     nombres_representante = models.TextField(blank=True, null=True)
     apellidos_representante = models.TextField(blank=True, null=True)
-    whatsapp_responsable = models.IntegerField(blank=True, null=True)
+    whatsapp_representante = models.IntegerField(blank=True, null=True)
     email_representante = models.TextField(blank=True, null=True)
     inscrito = models.CharField(blank=True, null=True, max_length=255)
 
     class Meta:
-        db_table = 'estudiantes_tabla'
+        db_table = 'estudiantes'
 
 
 class SalonTabla(models.Model):
@@ -91,6 +93,7 @@ class SalonTabla(models.Model):
 
 
 class SalonKpiModulo(models.Model):
+    id_skm = models.IntegerField(primary_key=True, unique=True, db_column='id_skm')
     uuid_salon = models.ForeignKey(SalonTabla, on_delete=models.CASCADE, db_column='uuid_salon')
     id_mol = models.ForeignKey(Modulos, on_delete=models.CASCADE, db_column='id_mol')
     grado = models.TextField(blank=True, null=True)
@@ -107,6 +110,7 @@ class SalonKpiModulo(models.Model):
 
 
 class Estatus(models.Model):
+    id_s = models.IntegerField(primary_key=True, unique=True, db_column='id_s')
     id_thinkific = models.ForeignKey(EstudiantesTabla, on_delete=models.CASCADE, db_column='id_thinkific')
     modulo = models.TextField(blank=True, null=True)
     email = models.TextField(blank=True, null=True, db_column='email')
@@ -135,8 +139,9 @@ class Estatus(models.Model):
 
 
 class Monitoreo(models.Model):
-    id_thinkific = models.ForeignKey(EstudiantesTabla, null=True, blank=True, on_delete=models.CASCADE, db_column='id_thinkific')
+    id_m = models.IntegerField(primary_key=True, unique=True)
     id_thinki_mon = models.ForeignKey(MonitorTabla, null=True, blank=True, on_delete=models.CASCADE, db_column='id_thinki_mon')
+    tiempo_sesion = models.IntegerField(blank=True, null=True)
     sign_in = models.TimeField(blank=True, null=True)
     sign_out = models.TimeField(blank=True, null=True)
     dia = models.DateField(blank=True, null=True)
@@ -146,10 +151,11 @@ class Monitoreo(models.Model):
 
 
 class Feedback(models.Model):
-    id_thinkific = models.ForeignKey(EstudiantesTabla, null=True, blank=True, on_delete=models.CASCADE, db_column='id_thinkific')
+    id_f = models.IntegerField(primary_key=True)
     id_thinki_mon = models.ForeignKey(MonitorTabla, null=True, blank=True, on_delete=models.CASCADE, db_column='id_thinki_mon')
     feedback = models.TextField(blank=True, null=True, db_column='feedback')
     errores = models.TextField(blank=True, null=True)
+    fecha = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         db_table = 'feedback'
