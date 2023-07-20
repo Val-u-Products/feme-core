0c1a9f1bb791e4db556cf5ad671891fbe6d184d5
Jaifran Hernández <jaifran@myval-u.com>
2023-07-20 15:04:24 -0400

    Update, GOD HELP ME!

--- src/feme_core/settings.py
+++ src/feme_core/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
 # import environ
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
