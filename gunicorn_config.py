301e90344cce7880c9fd81f67146adb670b3c644
Jaifran Hernández <jaifran@myval-u.com>
2023-07-25 02:52:28 -0400

    gunnicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,16 @@
 # gunicorn_config.py
 
-bind = '127.0.0.1:8000'
-workers = 3  # You can adjust the number of workers based on your server resources
+import multiprocessing
 
+# Gunicorn configuration
+bind = "0.0.0.0:8000"  # The address and port to bind Gunicorn to
+workers = multiprocessing.cpu_count() * 2 + 1  # The number of worker processes
+worker_class = "sync"  # The type of worker process to use (sync, gthread, etc.)
+timeout = 60  # Timeout for handling requests
+max_requests = 1000  # Number of requests a worker will process before restarting
+graceful_timeout = 30  # Timeout for graceful worker shutdown
+errorlog = "-"  # Log to stdout
+
+# Django-specific settings
+# Change to the project directory
+django_settings = "./src/feme_core/settings.py"  # Set the Django settings module
